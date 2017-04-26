ddb68b2ea5bedb0a41e2d465c850c1e6038a9c90
damioyedele <oluwadamilolaoyedele@Oluwadamilolas-MacBook-Air.local>
2017-04-26 17:27:33 -0500

    commit 4/26

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -2,18 +2,24 @@ class ProgramsController < ApplicationController
   def home
     # Your code goes below.
 
-    @your_output = "Replace this string with your output"
+    @your_output = 1 < 2
 
     render("programs/home.html.erb")
   end
 
   def first_program
-    # Your code goes below.
+    the_number = rand(100)
 
-    @your_output = "Replace this string with your output"
-
-    render("programs/first_program.html.erb")
+  if the_number < 25
+    @your_output = "It's going to be your lucky day today"
+  elsif the_number > 75
+    @your_output = "Don't leave home today"
+  else
+    @your_output = "It'll be an okay day today"
   end
+
+  render("programs/first_program.html.erb")
+end
 
   def second_program
     # Your code goes below.
